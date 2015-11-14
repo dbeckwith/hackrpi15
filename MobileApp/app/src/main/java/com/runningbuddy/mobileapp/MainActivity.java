0758fb031bb921fe7c3d6cc270f5c94353246a95
Daniel Beckwith <djbsnx@gmail.com>
2015-11-14 15:25:30 -0500

    Added mobile app project.

---
+++ MobileApp/app/src/main/java/com/runningbuddy/mobileapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.runningbuddy.mobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button)
+    Button button;
+    @Bind(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(textView.getText() + "!");
+            }
+        });
+    }
+}

3cce17ca7ee7565fb945bc90aa0176a70725e3c0
Daniel Beckwith <djbsnx@gmail.com>
2015-11-14 23:30:37 -0500

    submitRun now calculates user metrics after adding the new run

--- apiserver/submitRun.js
+++ apiserver/submitRun.js
@@ -14,7 +14,36 @@ function submitRun(body, callback) {
       var useractivity = db.collection('useractivity');
 
       useractivity.update({ 'userName': userName }, { $push: { 'runs': runInfo } });
-      callback();
+
+      useractivity.findOne({ 'userName': userName }, function (err, user) {
+        if (!err && user) {
+          var metric = {};
+
+          metric.timeOfDay = 0;
+          metric.distance = 0;
+          metric.speed = 0;
+          metric.calories = 0;
+
+          _.forEach(user.runs, function (run) {
+            metric.timeOfDay += run.timestamp % (1000 * 60 * 60 * 24);
+            metric.distance += run.distance;
+            metric.speed += run.speed;
+            metric.calories += run.calories / run.distance;
+          });
+
+          metric.timeOfDay /= user.runs.length;
+          metric.distance /= user.runs.length;
+          metric.speed /= user.runs.length;
+          metric.calories /= user.runs.length;
+
+          useractivity.update({ 'userName': userName }, { $set: { 'metric': metric } });
+
+          callback();
+        }
+        else {
+          console.log('error calculating user metric: ' + err);
+        }
+      });
     });
 }
 

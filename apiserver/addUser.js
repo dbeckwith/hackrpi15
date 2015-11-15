9d86079cd1c6e46f536dcd3a0d99a9f4b730ff9e
Aditya Nivarthi <adityanivarthi19@gmail.com>
2015-11-14 22:28:40 -0500

    Added addUser.

--- apiserver/addUser.js
+++ apiserver/addUser.js
@@ -1,3 +1,4 @@
+var dbClient = require('./mongodbsetup');
 
 /*
  * Add new user to database
@@ -6,6 +7,12 @@
  */
 function addUser(body, callback) {
 	var accountName = body.accountName;
+
+	dbClient(function (db) {
+		db.collection('activitybuddy').insert(
+			{'userName': accountName, 'runs': [], 'metrics': null},
+			callback);
+	});
 }
 
 modules.export = addUser;
